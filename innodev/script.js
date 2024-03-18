24d87e865f969fd1abd695877bf36c15778dadd0
Anushka <Anushkaprajapati2274@gmail.com>
2024-03-18 11:11:26 +0530

    changes

--- innodev/script.js
+++ innodev/script.js
@@ -2,3 +2,8 @@ function scrollToElement() {
     var element = document.getElementById("features");
     element.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToElement1() {
+    var element = document.getElementById("footer");
+    element.scrollIntoView({ behavior: "smooth" });
+}
